7b5f6a994aa744828a1d75eb7b67f1f45a6ac2d1
Mwabisi <pmwabisiradebe@gmail.com>
2023-07-31 12:49:56 +0200

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = int(input("Enter a number:"))
 last_digit = abs (number) %10
 print("The string last digit of", number, "is", last_digit)
 if last_digit > 5:
