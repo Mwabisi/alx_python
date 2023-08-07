d7da8f7dff439620b9af838af8d80b0bca8edaed
Mwabisi <pmwabisiradebe@gmail.com>
2023-08-07 12:19:36 +0200

    Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,20 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print("Matrix is empty")
-        return
-
-    max_width = max(len(str(num)) for row in matrix for num in row)
-
     for row in matrix:
         for num in row:
-            print("{:>{width}}".format(num, width=max_width), end=" ")
+            print("{:d}".format(num), end=" ")
         print()
-
-# Example usage
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
