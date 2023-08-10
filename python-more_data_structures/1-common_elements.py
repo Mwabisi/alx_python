f4a48d0e419f888a2413675600391af204cd9e5f
Mwabisi <pmwabisiradebe@gmail.com>
2023-08-10 17:48:36 +0200

    Present in both

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
-def common_elements(set_1, set_2):
-    common=set()
-set_1 ={1,2,3,4,5}
-set_2 ={2,3,5,6,7}
+set_1 = {"Python", "C", "Javascript"}
+set_2 = {"Bash", "C", "Ruby", "Perl"}
+
+common_elements = common_elements(set_1, set_2)
 print(common_elements)
