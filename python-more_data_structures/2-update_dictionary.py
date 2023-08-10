341da467db0a48dd7cfc0e95ebdcfa831ebd96e6
Mwabisi <pmwabisiradebe@gmail.com>
2023-08-10 18:38:55 +0200

    Update dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,23 +1,40 @@
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
-
 def print_sorted_dictionary(my_dict):
-    """ Print sorted dictionary """
-    keys = sorted(my_dict.keys())
-    for k in keys:
-        print("{}: {}".format(k, my_dict[k]))
+  """ Print sorted dictionary """
+  keys = sorted(my_dict.keys())
+  for k in keys:
+    print("{}: {}".format(k, my_dict[k]))
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary.copy(), 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+def update_dictionary(a_dictionary, key, value):
+  """Replaces or adds key/value in a dictionary.
 
-print("--")
-print("--")
+  Args:
+    a_dictionary: A dictionary.
+    key: A string.
+    value: Any type.
 
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+  Returns:
+    The updated dictionary.
+  """
+
+  if key in a_dictionary:
+    a_dictionary[key] = value
+  else:
+    a_dictionary[key] = value
+
+  return a_dictionary
+
+
+if __name__ == "__main__":
+  a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+  new_dict = update_dictionary(a_dictionary, 'language', "Python")
+  print_sorted_dictionary(new_dict)
+  print("--")
+  print_sorted_dictionary(a_dictionary)
+
+  print("--")
+  print("--")
+
+  new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+  print_sorted_dictionary(new_dict)
+  print("--")
+  print_sorted_dictionary(a_dictionary)
