d0dd68f2b7ba258e4028f9eaa06be913f2fa5663
Mwabisi <pmwabisiradebe@gmail.com>
2023-08-10 21:24:34 +0200

    Present in both

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,10 +1,20 @@
 #!usr/bin/python3
+
 def common_elements(set_1, set_2):
     return list(set_1 & set_2)
 
-# Example usage
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
+test_cases = [
+    ({"Python", "C", "Javascript"}, {"Bash", "C", "Ruby", "Perl"}),
+    ({"Python", "Javascript"}, {"Bash", "C", "Ruby", "Perl"}),
+    ({"Python", "Javascript"}, {"Python", "Javascript"}),
+    ({"Python"}, {"Bash"}),
+    ({"Python"}, {"Python"}),
+    ({"Python", "C", "Javascript"}, set()),
+    (set(), {"Python", "C", "Javascript"}),
+    (set(), set())
+]
 
-common_elements_list = common_elements(set_1, set_2)
-print(common_elements_list)
+# Perform tests and print results
+for idx, (set_1, set_2) in enumerate(test_cases, start=1):
+    common_elements_list = common_elements(set_1, set_2)
+    print(f"Correct output - case {idx}: {common_elements_list}")
