3bb8a0ee316337d2f65f7584c1bd5708834548d2
Mwabisi <pmwabisiradebe@gmail.com>
2023-08-10 19:41:53 +0200

    Multiply by using map

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,18 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    max_key = None
-    max_value = float('-inf')  # Initialize with negative infinity
-
-    for key, value in a_dictionary.items():
-        if isinstance(value, int) and value > max_value:
-            max_key = key
-            max_value = value
-
+    max_key = max(a_dictionary, key=a_dictionary.get)
     return max_key
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
