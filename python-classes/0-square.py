ce32bd92ec2ad7ef02f606043fb80cee9847a676
Mwabisi <pmwabisiradebe@gmail.com>
2023-08-14 13:59:02 +0200

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,12 @@
-#!/user/bin/phython3
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
     def __init__(self, size):
-        self.__size = size  # Private instance attribute
-
-    def __str__(self):
-        return f"<class '{self.__module__}.{type(self).__name__}'>\n{self.__dict__}\n'Square' object has no attribute 'size'\n'Square' object has no attribute '__size'"
-
-# Example usage
-square = Square(3)
-print(square)
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
