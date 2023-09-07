ec0e0ce075f4eaeb78fcbd6f7b6a74aec4dc72b4
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-07 10:04:46 +0200

    Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Defines class BaseGeometry"""
+
+    def area(self):
+        """Definition of area method"""
+        raise Exception("area() is not implemented")
