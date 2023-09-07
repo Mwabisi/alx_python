d06ae5de9e121119191d1767ceef4aeb3163b635
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-07 15:21:14 +0200

    Full rectangle

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -28,3 +28,26 @@ class Rectangle(BaseGeometry):
 
         area = self.__width * self.__height
         return area
+
+    def __str__(self):
+        """
+        Returns string representation of an instance of class rectangle
+        """
+        return "[{}] {}/{}".format(type(self).__name__,
+                                   self.__width, self.__height)
+class Rectangle(BaseGeometry):
+    """
+        Class Rectangle extends from  BaseGeometry
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__width, self.__height)
