0ce5d76ef6300ec20f978db61b6d2e6b53d54abc
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-08 14:33:05 +0200

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,8 +6,13 @@ url = "https://intranet.hbtn.io/status"
 response = requests.get(url)
 
 if response.status_code == 200:
+    data_type = type(response.text).__name__
     content = response.text
+    content_length = len(content)
+    
     print("Body response:")
-    print("\t- content:", content)
+    print(f"\t- type: {data_type}")
+    print(f"\t- content: {content}")
+    print(f"({content_length} chars long)")
 else:
-    print(f"Error: Unable to fetch the URL. Status code: {response.status_code}") 
+    print(f"Error: Unable to fetch the URL. Status code: {response.status_code}")
