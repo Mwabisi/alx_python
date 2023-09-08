3f551579d6b8938b72811110cc05376d01f05fb5
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-08 13:59:37 +0200

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """script that fetches 'https://alx-intranet.hbtn.io/status' """
-if __name__ == "__main__":
-    import urllib.request
+import requests
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
-            as response:
-        html = response.read()
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
     print("Body response:")
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print("Error: Unable to fetch the URL. Status code:", response.status_code)
