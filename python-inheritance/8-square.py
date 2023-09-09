f12dd0623bf194ca97b9993fb16c9873c1d99168
Mwabisi <pmwabisiradebe@gmail.com>
2023-09-09 21:56:30 +0200

    multiple files

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -3,21 +3,18 @@
 Module for class Square.
 """
 
-Rectangle = __import__('7-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-        Class Square extends from  Rectangle
-    """
+    """Class Square that inherits from class Rectangle"""
+
     def __init__(self, size):
+        """Initialize an instance of the class Square"""
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        # super().__init__(size, size)
 
     def area(self):
+        """Return the area of Square"""
         return self.__size ** 2
-
-    def __str__(self):
-        return "[{}] {}/{}".format(self.__class__.__name__,
-                                   self.__size, self.__size)
