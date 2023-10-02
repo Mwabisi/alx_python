49a01c444275b33e2b5e3eccfcee19b937b15971
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-02 12:18:19 +0200

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,70 +1,45 @@
 import requests
+import sys
 
-def get_employee_todo_list_progress(employee_id):
-  """Gets the TODO list progress for an employee with the given employee ID.
+def fetch_employee_data(employee_id):
+    # URL for employee details
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 
-  Args:
-    employee_id: The employee ID.
+    # URL for employee's TODO list
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-  Returns:
-    A dictionary containing the following information about the employee's TODO
-    list progress:
-      * name: The employee's name.
-      * number_of_done_tasks: The number of completed tasks.
-      * total_number_of_tasks: The total number of tasks.
-      * completed_tasks: A list of the titles of the completed tasks.
-  """
+    try:
+        # Fetch employee details
+        employee_response = requests.get(employee_url)
+        employee_response.raise_for_status()
+        employee_data = employee_response.json()
 
-  # Get the employee's TODO list items.
-  todo_list_items_endpoint = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-  todo_list_items_response = requests.get(todo_list_items_endpoint)
-  todo_list_items = todo_list_items_response.json()
+        # Fetch TODO list
+        todo_response = requests.get(todo_url)
+        todo_response.raise_for_status()
+        todo_data = todo_response.json()
 
-  # Get the employee's name.
-  employee_details_endpoint = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-  employee_details_response = requests.get(employee_details_endpoint)
-  employee_details = employee_details_response.json()
-  employee_name = employee_details.get("name")
-
-  # Calculate the number of completed and non-completed tasks.
-  number_of_done_tasks = 0
-  number_of_non_completed_tasks = 0
-  for todo_list_item in todo_list_items:
-    if todo_list_item.get("completed"):
-      number_of_done_tasks += 1
-    else:
-      number_of_non_completed_tasks += 1
-
-  # Calculate the total number of tasks.
-  total_number_of_tasks = number_of_done_tasks + number_of_non_completed_tasks
-
-  # Get the titles of the completed tasks.
-  completed_tasks = []
-  for todo_list_item in todo_list_items:
-    if todo_list_item.get("completed"):
-      completed_tasks.append(f"Task {todo_list_item.get('id')}: {todo_list_item.get('title')}")
-
-  # Return a dictionary containing the employee's TODO list progress.
-  return {
-    "name": employee_name,
-    "number_of_done_tasks": number_of_done_tasks,
-    "total_number_of_tasks": total_number_of_tasks,
-    "completed_tasks": completed_tasks
-  }
-
+        return employee_data, todo_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 def main():
-  # Get the employee ID from the user.
-  employee_id = int(input("Enter the employee ID: "))
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-  # Get the employee's TODO list progress.
-  employee_todo_list_progress = get_employee_todo_list_progress(employee_id)
+    employee_id = int(sys.argv[1])
+    employee_data, todo_data = fetch_employee_data(employee_id)
 
-  # Display the employee's TODO list progress on the standard output.
-  print(f"Employee {employee_todo_list_progress['name']} is done with tasks({employee_todo_list_progress['number_of_done_tasks']}/{employee_todo_list_progress['total_number_of_tasks']}):")
-  for completed_task in employee_todo_list_progress["completed_tasks"]:
-    print(f"     {completed_task}")
+    employee_name = employee_data.get("name")
+    completed_tasks = [task["title"] for task in todo_data if task["completed"]]
+    total_tasks = len(todo_data)
 
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    
+    for task in completed_tasks:
+        print(f"\t{task}")
 
 if __name__ == "__main__":
-  main()
+    main()
