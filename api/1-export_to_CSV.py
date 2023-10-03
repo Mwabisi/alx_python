45931f8b75a82df3e7660c9a33de3a83ada76cea
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-03 13:17:16 +0200

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,41 +1,51 @@
+import os
 import csv
-import requests
-import sys
 
+# Function to perform checks
+def perform_checks(employee_id):
+    # Check file existence
+    csv_filename = f"{employee_id}.csv"
+    if not os.path.exists(csv_filename):
+        print("CSV file does not exist.")
+        return
 
-def fetch_employee_data(employee_id):
-    # Define the API endpoints
+    # Check file naming convention
+    if not csv_filename.endswith(f"{employee_id}.csv"):
+        print("Incorrect CSV file name.")
+        return
+
+    # Check correct number of tasks
+    with open(csv_filename, mode='r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        task_count = sum(1 for _ in csv_reader) - 1  # Subtract 1 for the header row
+        expected_task_count = 11  # Adjust this to the expected number of tasks
+        if task_count != expected_task_count:
+            print("Incorrect number of tasks in the CSV.")
+            return
+
+    # Fetch user data from API for comparison
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(user_url)
+    user_data = response.json()
 
-    try:
-        # Fetch user data
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
+    # Check correct user ID and username
+    with open(csv_filename, mode='r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip the header row
+        for row in csv_reader:
+            user_id, username, _, _ = row
+            if user_id != str(employee_id) or username != user_data['username']:
+                print("Incorrect user ID and username in the CSV.")
+                return
 
-        # Fetch TODO list data
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
+    # Check correct output formatting
+    # Implement formatting checks here
 
-        # Create and write data to CSV file
-        csv_filename = f"{user_data['id']}.csv"
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            # Write the header row
-            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            # Write task records
-            for task in todos_data:
-                csv_writer.writerow([user_data['id'], user_data['username'], task['completed'], task['title']])
-
-        print(f"Data has been exported to {csv_filename}.")
-
-    except requests.exceptions.RequestException as e:
-        print("Error: Unable to fetch data from the API.")
-        print(e)
+    print("All checks passed successfully.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+        print("Usage: python checker.py <employee_id>")
     else:
         employee_id = int(sys.argv[1])
-        fetch_employee_data(employee_id)
+        perform_checks(employee_id)
