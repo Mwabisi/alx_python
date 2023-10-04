0bcbf70dd064a985b00c50fe83a31c2f466b8f58
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-04 11:59:48 +0200

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,34 +1,53 @@
-import os
+import csv
+import requests
+import sys
 
-def export_employee_TODO_list_to_CSV(employee_data, todos_data):
-  """Exports the employee's TODO list data to a CSV file.
+def get_user_tasks(user_id):
+    # Define the URL for the JSONPlaceholder API
+    base_url = 'https://jsonplaceholder.typicode.com'
+    
+    # Send a GET request to retrieve user data
+    user_url = f'{base_url}/users/{user_id}'
+    user_response = requests.get(user_url)
+    
+    # Check if the user exists
+    if user_response.status_code != 200:
+        print(f"User with ID {user_id} not found.")
+        return
+    
+    user_data = user_response.json()
+    username = user_data['username']
 
-  Args:
-    employee_data: A dictionary containing the employee's data.
-    todos_data: A list of dictionaries containing the employee's TODO list items.
-  """
+    # Send a GET request to retrieve user's tasks
+    tasks_url = f'{base_url}/todos?userId={user_id}'
+    tasks_response = requests.get(tasks_url)
+    
+    # Check if tasks were retrieved successfully
+    if tasks_response.status_code != 200:
+        print(f"Failed to retrieve tasks for user with ID {user_id}.")
+        return
+    
+    tasks_data = tasks_response.json()
+    
+    # Create a CSV file to store the user's tasks
+    with open(f'{user_id}.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        
+        # Write the CSV header
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        
+        for task in tasks_data:
+            task_title = task['title']
+            task_completed = task['completed']
+            
+            # Write the task data to the CSV file
+            csv_writer.writerow([str(user_id), username, str(task_completed), task_title])
+    
+    print(f'Tasks for user with ID {user_id} exported to {user_id}.csv successfully.')
 
-  # Create a new CSV file
-  csv_file_path = f"{employee_data['id']}.csv"
-
-  # Check if the CSV file exists
-  if not os.path.exists(csv_file_path):
-    # Create the CSV file
-    with open(csv_file_path, "w", newline="") as csv_file:
-      csv_writer = csv.writer(csv_file)
-
-      # Write the header row to the CSV file
-      csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-  # Write the employee's TODO list data to the CSV file
-  with open(csv_file_path, "a", newline="") as csv_file:
-    csv_writer = csv.writer(csv_file)
-
-    for task in todos_data:
-      csv_writer.writerow([
-        employee_data["id"],
-        employee_data["username"],
-        task["completed"],
-        task["title"],
-      ])
-
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 export_to_CSV.py <USER_ID>')
+    else:
+        user_id = sys.argv[1]
+        get_user_tasks(user_id)
