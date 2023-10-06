c0289c98217af812da32667f158f3afa430c83cf
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-06 13:37:10 +0200

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,53 +1,52 @@
+"""
+This module gathers and prints information about the task completion status of an employee.
+The employee data is obtained from an API endpoint.
+"""
+
 import json
 import requests
 import sys
 
-def user_info(employee_id):
+def get_employee_data(employee_id):
     """
-    Fetches user information and their TODO list for a given employee ID and exports it to a JSON file.
+    Fetches data about the employee and their tasks from an API.
 
     Args:
-        employee_id (int): The ID of the employee for whom to fetch data.
+    employee_id (int): The ID of the employee.
 
     Returns:
-        None
+    dict: The information about the employee.
+    dict: The tasks of the employee.
     """
-    # API endpoint URLs
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
+    todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos').json()
+    
+    return user_info, todos
 
-    # Fetch user data from the API
-    response_user = requests.get(user_url)
-    user_data = response_user.json()
-    username = user_data["username"]
+def write_to_json(user_info, todos):
+    """
+    Writes the tasks of an employee to a JSON file.
 
-    # Fetch todo data from the API
-    response_todo = requests.get(todo_url)
-    todo_data = response_todo.json()
-
-    # Create a list of tasks with the required format
-    tasks = [
-        {
-            "task": task["title"],
-            "completed": task["completed"],
-            "username": username
-        }
-        for task in todo_data
-    ]
-
-    # Create a dictionary with the user ID as the key and tasks list as the value
-    user_info_dict = {
-        str(employee_id): tasks
-    }
-
-    # Export data to JSON file
-    json_filename = f"{employee_id}.json"
-    with open(json_filename, 'w') as json_file:
-        json.dump(user_info_dict, json_file, indent=4)
+    Args:
+    user_info (dict): The information about the employee.
+    todos (dict): The tasks of the employee.
+    """
+    
+    filename = f"{user_info.get('id')}.json"
+    tasks = []
+    for task in todos:
+        tasks.append({
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': user_info.get('username')
+        })
+    with open(filename, 'w') as jsonfile:
+        json.dump({user_info.get('id'): tasks}, jsonfile)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python main.py <employee_id>")
+        print("Usage: python3 1-export_to_JSON.py <employee_id>")
     else:
-        employee_id = int(sys.argv[1])
-        user_info(employee_id)
+        user_info, todos = get_employee_data(sys.argv[1])
+        write_to_json(user_info, todos)
