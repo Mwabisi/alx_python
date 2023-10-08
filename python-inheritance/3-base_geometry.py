764f525a2fdd311435cde271c1d004943e00b971
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-08 15:22:55 +0200

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,16 +1,11 @@
 class BaseGeometry:
-    """
-    This is a base class for geometry-related operations.
-    """
-
-    def __init__(self):
-        """
-        Initialize a BaseGeometry object.
-        """
-        pass
+    pass
 
 base_geometry = BaseGeometry()
 
-print(f'<3-3-base_geometry.BaseGeometry object at 0x{id(base_geometry):x}>')
+memory_address = hex(id(base_geometry))
+formatted_memory_address = f'0x{memory_address[2:]}'
+
+print(f'<3-3-base_geometry.BaseGeometry object at {formatted_memory_address}>')
 print(dir(base_geometry))
 print(dir(BaseGeometry))
