90284a6e0b0ddc1abef6846265058a52cd84c6cd
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-08 14:26:43 +0200

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,9 +1,12 @@
-"""
-This module defines an empty class named BaseGeometry
-"""
-
 class BaseGeometry:
     """
-    This is an empty class named BaseGeometry
+    This is an empty class named BaseGeometry.
     """
-    pass
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+base_geometry = BaseGeometry()
+
+print(base_geometry)
+print(base_geometry.__dict__)
