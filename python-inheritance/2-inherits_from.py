2b3593ce97b5b9eb30dcae6a4214fc72fcabbc74
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-08 13:54:01 +0200

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,3 +1,16 @@
-def is_same_class(obj, a_class):
-  """Returns True if the object is exactly an instance of the specified class; otherwise False."""
-  return obj.__class__ == a_class
+"""
+This module checks if an object is an instance of a class that was inherited (directly or indirectly) from the specified class.
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Checks if an object is an instance of a class that was inherited (directly or indirectly) from the specified class.
+
+    Parameters:
+    obj : The object to check
+    a_class : The class to check against
+
+    Returns:
+    True if the object is an instance of a class that was inherited from the specified class. False otherwise.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
