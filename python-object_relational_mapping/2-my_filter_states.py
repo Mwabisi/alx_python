eb46ba94995fa71b8ddeca013845d0ef2f3df074
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-09 15:11:03 +0200

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,50 @@
-#!/usr/bin/python3
 import MySQLdb
 import sys
 
+def search_states(username, password, database_name, search_name):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+        )
 
-def main():
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+
+        # Prepare the SQL query with user input using format
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        query = query.format(search_name)
+
+        # Execute the SQL query
+        cursor.execute(query, (search_name,))
+
+        # Fetch all the matching rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+        # Close the cursor and the database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database_name> <state_name>")
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
-    dbname = sys.argv[3]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=dbname)
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (state_name,))
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
-
-
-if __name__ == '__main__':
-    main()
+    search_states(username, password, database_name, state_name)
