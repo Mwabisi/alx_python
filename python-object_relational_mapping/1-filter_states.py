87c602d39920ea45fe869f21886b32d451289e1d
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-09 13:08:13 +0200

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,54 +1,29 @@
+#!/usr/bin/python3
 import MySQLdb
 import sys
 
 
-def list_states_with_name_starting_with_n(username, password, database_name):
-    """
-    Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
 
-    Args:
-        username: The MySQL username.
-        password: The MySQL password.
-        database_name: The database name.
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=dbname)
 
-    Returns:
-        A list of states with a name starting with N (upper N), sorted in ascending order by states.id.
-    """
+    cur = db.cursor()
 
-    # Connect to the MySQL server.
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database_name,
-    )
-
-    # Create a cursor object to interact with the database.
-    cursor = db.cursor()
-
-    # Execute the SQL query to select states with names starting with 'N'.
-    cursor.execute("SELECT * FROM states WHERE LOWER(name) LIKE 'n%' ORDER BY id ASC")
-
-    # Fetch all the matching rows.
-    rows = cursor.fetchall()
-
-    # Display the results.
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection.
-    cursor.close()
+    cur.close()
     db.close()
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    list_states_with_name_starting_with_n(username, password, database_name)
+if __name__ == '__main__':
+    main()
