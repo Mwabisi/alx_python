3cbc47fa214f3bf596049272e0187993c97cb252
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-09 11:53:49 +0200

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,52 +3,52 @@ import sys
 
 
 def list_states_with_name_starting_with_n(username, password, database_name):
-  """
-  Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+    """
+    Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
 
-  Args:
-    username: The MySQL username.
-    password: The MySQL password.
-    database_name: The database name.
+    Args:
+        username: The MySQL username.
+        password: The MySQL password.
+        database_name: The database name.
 
-  Returns:
-    A list of states with a name starting with N (upper N), sorted in ascending order by states.id.
-  """
+    Returns:
+        A list of states with a name starting with N (upper N), sorted in ascending order by states.id.
+    """
 
-  # Connect to the MySQL server.
-  db = MySQLdb.connect(
-      host="localhost",
-      port=3306,
-      user=username,
-      passwd=password,
-      db=database_name,
-  )
+    # Connect to the MySQL server.
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name,
+    )
 
-  # Create a cursor object to interact with the database.
-  cursor = db.cursor()
+    # Create a cursor object to interact with the database.
+    cursor = db.cursor()
 
-  # Execute the SQL query to select states with names starting with 'N'.
-  cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute the SQL query to select states with names starting with 'N'.
+    cursor.execute("SELECT * FROM states WHERE LOWER(name) LIKE 'n%' ORDER BY id ASC")
 
-  # Fetch all the matching rows.
-  rows = cursor.fetchall()
+    # Fetch all the matching rows.
+    rows = cursor.fetchall()
 
-  # Display the results.
-  for row in rows:
-    print(row)
+    # Display the results.
+    for row in rows:
+        print(row)
 
-  # Close the cursor and the database connection.
-  cursor.close()
-  db.close()
+    # Close the cursor and the database connection.
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":
-  if len(sys.argv) != 4:
-    print("Usage: python script.py <username> <password> <database_name>")
-    sys.exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database_name>")
+        sys.exit(1)
 
-  username = sys.argv[1]
-  password = sys.argv[2]
-  database_name = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
 
-  list_states_with_name_starting_with_n(username, password, database_name)
+    list_states_with_name_starting_with_n(username, password, database_name)
