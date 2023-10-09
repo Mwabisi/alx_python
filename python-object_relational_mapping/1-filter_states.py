131fe350d89651a089dd7263a99827d9d38a421a
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-09 14:15:38 +0200

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,6 @@ def main():
     cur.execute("SELECT * FROM states "
                 "WHERE name LIKE BINARY 'N%' "
                 "ORDER BY states.id ASC")
-    
     rows = cur.fetchall()
     for row in rows:
         print(row)
