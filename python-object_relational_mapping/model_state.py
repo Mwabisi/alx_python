951289ffc3ef72509dc6f6ac69a646730fec0e5b
Mwabisi <pmwabisiradebe@gmail.com>
2023-10-10 15:04:13 +0200

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,6 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """
+    State class to represent the 'states' table in the MySQL database.
+
+    Attributes:
+        id (int): An auto-generated, unique integer representing the primary key.
+        name (str): A string with a maximum of 128 characters, representing the state name.
+
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
